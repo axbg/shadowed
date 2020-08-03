fd97fcc2e4d537ce47e6da32170fca502f585305
axbg <bisagalexstefan@gmail.com>
2020-08-03 21:21:43 +0300

    Initialized Nuxt.js & implemented app structure

---
+++ components/PictureColumn.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <PictureFrame v-for="(picture, index) in pictures" :key="index" :picture="picture" /> 
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import PictureFrame from '~/components/PictureFrame.vue'
+
+@Component({
+  components: {
+    PictureFrame
+  }
+})
+export default class PictureColumn extends Vue {
+    @Prop({ type: Array, required: true })
+    pictures!: string[];
+
+    constructor() {
+        super();
+    }
+}
+</script>
---
+++ components/PictureFrame.vue
@@ -0,0 +1,19 @@
+<template>
+    <img :src="require('~/assets/pictures/' + picture)" />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class PictureFrame extends Vue {
+  @Prop({type: String, required: true})
+  picture!: string;
+
+  constructor() {
+    super();
+
+    console.log(this.picture);
+  }
+}
+</script>
